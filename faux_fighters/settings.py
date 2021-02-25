c40d4b519167cdf8e67a7ad75fc18082d7ff3259
SimonGeorgeNZ <simon.george@stjohn.org.nz>
2021-02-25 16:15:49 +0000

    Added contexts.py

--- faux_fighters/settings.py
+++ faux_fighters/settings.py
@@ -63,6 +63,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 # Required by allauth
                 'django.contrib.messages.context_processors.messages',
+                'bag.contexts.bag_contents',
             ],
         },
     },
@@ -144,3 +145,6 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+FREE_DELIVERY_THRESHOLD = 50
+STANDARD_DELIVERY_PERCENTAGE = 10
